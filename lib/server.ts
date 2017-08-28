aaf02978bd3a0b7a54133e7df7a201a27c9fe18d
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-28 15:18:06 -0400

    Configure pm2

--- lib/server.ts
+++ lib/server.ts
@@ -3,6 +3,11 @@ import { config } from './config';
 
 const app = express();
 app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.render('index', { answer: 42 });
+});
 
 app.listen(config.port, () => {
   console.info(`Running on ${config.port}`);
