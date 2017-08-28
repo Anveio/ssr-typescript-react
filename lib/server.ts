10424056c5e99ff3b77535a1dfdbb2a36ace75d3
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-28 14:31:56 -0400

    Initial commit.

---
+++ lib/server.ts
@@ -0,0 +1,9 @@
+import * as express from 'express';
+import { config } from './config';
+
+const app = express();
+app.use(express.static('public'));
+
+app.listen(config.port, () => {
+  console.info(`Running on ${config.port}`);
+});
