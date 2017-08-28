289d227614632632d1316b14c1950b4f4d165b65
Shovon Hasan <im.shovonhasan@gmail.com>
2017-08-28 18:06:24 -0400

    Edit gitignore.

--- lib/components/Index.tsx
+++ lib/components/Index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 export const App = () => {
-  return <h1>Hi everyone.</h1>;
+  return <h1>Hi everyone except you.</h1>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
